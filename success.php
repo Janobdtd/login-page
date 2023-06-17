930ffdab7157a2fa5e00707a86a1063fb3a944c9
Janob DTD <107302200+Janobdtd@users.noreply.github.com>
2023-06-17 19:27:33 +0500

    Create success.php

---
+++ success.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Muvaffaqiyatli !</title>
+    </head>
+    <body>
+        <h1 style="color: green; text-align: center;">Muvaffaqiyatli</h1>
+    </body>
+</html>
